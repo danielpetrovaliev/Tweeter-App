0c42e09b3ac546cfa2030797e433cb953e697840
petrovaliev95 <petrovaliev95@abv.bg>
2015-05-14 15:51:49 +0300

    added new properties

--- Tweeter/Tweeter.Models/Replay.cs
+++ Tweeter/Tweeter.Models/Replay.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
 namespace Tweeter.Models
 {
-    class Replay
+    public class Replay
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public int AuthorId { get; set; }
+
+        [Required]
+        public int TweetId { get; set; }
+
+        public Tweet Tweet { get; set; }
+
+        public User Author { get; set; }
     }
 }
